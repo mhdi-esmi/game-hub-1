ff49f336ac38ccad1f6bb3d2f7ab01a5792c3ddb
mhdi <m.esmaeili@gmail.com>
2024-02-15 17:45:58 +0330

    Refactoring fetch with react query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,11 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import { GameQuery } from "../App";
 import useGames, { Platform } from "../hooks/useGames";
 import { Genre } from "../hooks/useGenres";
@@ -11,10 +18,17 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const { data, isLoading, error } = useGames(gameQuery);
+
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error)
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        <AlertTitle>{error.message}!</AlertTitle>
+      </Alert>
+    );
 
   return (
     <SimpleGrid
@@ -28,7 +42,7 @@ const GameGrid = ({ gameQuery }: Props) => {
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
         </GameCardContainer>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "f4631d08167a4e0c8c8c0c53e3fb43b4",
   },
 });
