a8192c92d4f39568c977482883682c2b58be79b4
mhdi <m.esmaeili@gmail.com>
2024-02-18 22:21:56 +0330

    Build game attribute

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 
 
@@ -6,11 +8,12 @@ export interface Game {
   id: number;
   name: string;
   slug: string;
-  // genres: Genre[];
-  // publishers: Publisher[];
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform; }[];
   metacritic: number;
   rating_top: number;
+  
 }
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,13 +1,14 @@
-import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
 import { ExpandableText } from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
+import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
 
   const { data: game, isLoading, error } = useGame(slug!);
+  console.log(game);
 
   if (isLoading) return <Spinner />;
 
@@ -17,6 +18,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
